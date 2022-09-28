2d706604bf18f491a3e14638bf5a1551f139573b
Angelo <angelo.gonzalez@live.com>
2022-09-28 14:12:50 -0300

    370. Tarea: Validar campos

--- routes/controllers/auth.js
+++ routes/controllers/auth.js
@@ -3,7 +3,15 @@ const { validationResult } = require('express-validator');
 
 const crearUsuario = (req, res = response ) => {
 
-    console.log( req.body);
+
+    const errors = validationResult( req );
+
+    if( !errors.isEmpty() ) {
+        return res.status(400).json({
+            ok: false,
+            errors: errors.mapped()
+        });
+    }
 
     const { email, name, password } = req.body;
     console.log( email, name, password);
