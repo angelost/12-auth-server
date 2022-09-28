5bd931b3d44b632ad5c63bf22c8f738602e35ecf
Angelo <angelo.gonzalez@live.com>
2022-09-28 18:42:34 -0300

    375. Crear usuario en base de datos

---
+++ controllers/auth.js
@@ -0,0 +1,74 @@
+const { response } = require('express');
+const Usuario = require('../models/Usuario');
+
+const crearUsuario = async (req, res = response ) => {    
+
+    const { email, name, password } = req.body;
+
+
+    try {
+
+        // Verificar el email
+        const usuario = await Usuario.findOne({ email });
+
+        if ( usuario ) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'El usuario ya existe con ese email'
+            });
+        }
+
+        // Crear usuario con el modelo
+
+        const dbUser = new Usuario( req.body );
+
+
+
+        // Hashear la contraseña
+
+        // Generar el JWT
+
+        // Crear usuario de BD
+        await dbUser.save();
+
+        // Generar respuesta exitosa
+        return res.status(201).json({
+            ok: true,
+            uid: dbUser.id,
+            name,
+        });
+        
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            ok: false,
+            msg: 'Por favor hable con el administrador'
+        });        
+    }
+
+    
+}
+
+const loginUsuario = (req, res = response ) => {
+    
+    const { email, password } = req.body;
+
+    return res.json({
+        ok: true,
+        msg: 'Login de usuario /'
+    });
+}
+
+const revalidarToken = (req, res = response ) => {
+
+    return res.json({
+        ok: true,
+        msg: 'Renew'
+    });
+}
+
+module.exports = {
+    crearUsuario,
+    loginUsuario,
+    revalidarToken
+}
--- models/Usuario.js
+++ models/Usuario.js
@@ -1,6 +1,5 @@
 const { Schema, model } = require("mongoose");
 
-
 const UsuarioSchema = Schema({
     name: {
         type: String,
