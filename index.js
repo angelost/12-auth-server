2413894f0d007aecb038bc20e9013450e35512a1
Angelo <angelo.gonzalez@live.com>
2022-09-28 13:47:02 -0300

    368. Servir una página HTTP desde Express

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ require('dotenv').config();
 // Crear el servidor/aplicacion de express
 const app = express();
 
+// Directorio Publico
+app.use( express.static('public'));
+
 // CORS
 app.use( cors());
 
