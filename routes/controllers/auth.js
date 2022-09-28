c752ad0d38c8a74b8f97afd867ef2b381deee264
Angelo <angelo.gonzalez@live.com>
2022-09-28 16:52:22 -0300

    371. Custom Middleware - ValidarCampos

--- routes/controllers/auth.js
+++ routes/controllers/auth.js
@@ -1,20 +1,8 @@
 const { response } = require('express');
-const { validationResult } = require('express-validator');
 
-const crearUsuario = (req, res = response ) => {
-
-
-    const errors = validationResult( req );
-
-    if( !errors.isEmpty() ) {
-        return res.status(400).json({
-            ok: false,
-            errors: errors.mapped()
-        });
-    }
+const crearUsuario = (req, res = response ) => {    
 
     const { email, name, password } = req.body;
-    console.log( email, name, password);
 
     return res.json({
         ok: true,
@@ -23,19 +11,8 @@ const crearUsuario = (req, res = response ) => {
 }
 
 const loginUsuario = (req, res = response ) => {
-
-    const errors = validationResult( req );
-
-    if( !errors.isEmpty() ) {
-        return res.status(400).json({
-            ok: false,
-            errors: errors.mapped()
-        });
-    }
-
-
+    
     const { email, password } = req.body;
-    console.log( email, password);
 
     return res.json({
         ok: true,
