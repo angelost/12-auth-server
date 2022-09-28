dad5e04a1bc0370a8db8e6a21bf7a20fe7217675
Angelo <angelo.gonzalez@live.com>
2022-09-28 13:33:20 -0300

    367. Variables de entorno de Node

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+
 
 // Crear el servidor/aplicacion de express
 const app = express();
@@ -14,6 +16,6 @@ app.use( express.json());
 // Rutas
 app.use('/api/auth', require('./routes/auth'));
 
-app.listen( 4000, () => {
-    console.log(`Servidor corriendo en puerto ${ 4000 }`);
+app.listen( process.env.PORT, () => {
+    console.log(`Servidor corriendo en puerto ${ process.env.PORT }`);
 });
